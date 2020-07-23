c2595e30b1b3fa7050bd8b708d658a3425074709
eunjoo jung <lynn0506@snu.ac.kr>
2020-07-24 00:55:56 +0900

    second setting-html

--- snuDorm/feedpage/views.py
+++ snuDorm/feedpage/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-# pylint: disable=relative-beyond-top-level
-from .models import Feed, Cobuy, Share, Store, Deal, FeedComment
-                    # FeedComment, FeedLike, FeedUnlike, CommentLike, \
-                    # CommentUnlike, CommentReply
+from .models import Feed, Cobuy, Share, Store, Deal, FeedComment \
+                    FeedComment, FeedLike, FeedUnlike, CommentLike, \
+                    CommentUnlike, CommentReply
 from django.contrib.auth.models import User
 from django.shortcuts import redirect 
 
@@ -15,3 +14,19 @@ def index(request):
     elif request.method == 'POST': 
         
         return redirect('/feeds')
+
+# def post(request):
+#     if request.method == 'GET':
+
+
+#         return render(request, 'feedpage/post.html')
+    
+#     redirect('/feeds')
+
+# def show(request):
+#     if request.method == 'POST':
+
+
+#         return render(request, 'feedpage/show.html')
+    
+#     redirect('/feeds')
