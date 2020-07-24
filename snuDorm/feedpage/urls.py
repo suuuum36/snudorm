0bc9de70d006a35a77e7d995af91c32b491c133a
eunjoo jung <lynn0506@snu.ac.kr>
2020-07-25 00:57:13 +0900

    models 상속 사용 & views, template 간소화

--- snuDorm/feedpage/urls.py
+++ snuDorm/feedpage/urls.py
@@ -9,13 +9,10 @@ urlpatterns = [
     path('<int:id>/edit/', views.edit, name='edit'),
     path('<int:id>/delete/', views.delete, name='delete'),
     path('<int:id>/comment/', views.comment, name='comment'),
-    path('<int:id>/new', views.new, name='new'),
+    path('<int:id>/new/', views.new, name='new'),
+    path('<int:id>/show/', views.show, name='show'),
 
     path('minwon/', views.minwon, name='minwon'),
-    path('cobuy/', views.cobuy, name='cobuy'),
-    path('share/', views.share, name='share'),
-    path('store/', views.store, name='store'),
-    path('deal/', views.deal, name='deal'),
     path('market/', views.market, name='market'),
     path('freeboard/', views.freeboard, name='freeboard'),
 ]
