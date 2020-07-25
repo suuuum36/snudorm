2a654510db3c6e48c504aa047135ca859547c142
eunjoo jung <lynn0506@snu.ac.kr>
2020-07-25 23:53:39 +0900

    models & views & urls midified by juju

--- snuDorm/accounts/models.py
+++ snuDorm/accounts/models.py
@@ -11,24 +11,17 @@ from faker import Faker
 # Create your models here.
 class Profile(models.Model):  
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # nickname = models.CharField(max_length=15, blank=True)
-    college = models.CharField(max_length=20, blank=True)
-    major = models.CharField(max_length=20, blank=True)
-    email = models.CharField(max_length=50, blank=True)
-    birthday = models.DateField(blank=True, null=True)
+    nickname = models.CharField(max_length=15, blank=True)
 
-    # multiselect 
+    # TODO: multiselect 로 수정 필요
     building = models.CharField(max_length=20, blank=True)
-    # snu.ac.kr template 설정 필요 
+    # TODO: snu.ac.kr template 설정 필요 
     email = models.CharField(max_length=50, blank=True)
     
     # faker test 위해 number 지운상태
     def __str__(self):   
-        return f'id={self.id}, user_id={self.user.id}, \
-                college = {self.college}, major={self.major}, \
-                email={self.email}, birthday={self.birthday}, \
-                address={self.address}'
-                #number={self.number}
+        return f'id={self.id}, user_id={self.user.id}, nickname={self.nickname}, \
+                email={self.email}, building={self.building}' 
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):  
@@ -55,4 +48,4 @@ class Profile(models.Model):
             address = myfake.address()
 
             Profile.objects.filter(user=user).update(college=college,\
-                major=major, email=email, birthday=birthday, address=address
+                major=major, email=email, birthday=birthday, address=address)
