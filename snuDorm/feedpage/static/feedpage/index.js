5340dc110f22a8f94a428cd0c6a01c50e9195e52
eunjoo jung <lynn0506@snu.ac.kr>
2020-07-31 15:47:19 +0900

    index.html & index.js added 메인페이지 수정

---
+++ snuDorm/feedpage/static/feedpage/index.js
@@ -0,0 +1,61 @@
+// 공통게시판 - 주간 
+function gongWeek() {
+    console.log('공통 주간게시판 clicked');
+    $('#gong_week_lists').attr("style", "display:;");
+    $('#gong_day_lists').attr("style", "display:none;");
+}
+// 공통게시판 - 일간
+function gongDay() {
+    console.log('공통 일간게시판 clicked');
+    $('#gong_day_lists').attr("style", "display:;");
+    $('#gong_week_lists').attr("style", "display:none;");
+}
+// 동 게시판 - 주간
+function dongWeek() {
+    console.log('동 주간게시판 clicked');
+    $('#dong_week_lists').attr("style", "dispaly:;");
+    $('#dong_day_lists').attr("style", "display:none;");
+}
+// 동 게시판 - 일간 
+function dongDay() {
+    console.log('동 일간게시판 clicked');
+    $('#dong_day_lists').attr("style", "display:;");
+    $('#dong_week_lists').attr("style", "display:none;");
+}
+
+// 생활게시판 - 공구
+function cobuy() {
+    console.log('공구 게시판 clicked');
+    $('#cobuy_lists').attr("style", "display:;");
+    $('#keep_lists').attr("style", "display:none;");
+    $('#rent_lists').attr("style", "display:none;");
+    $('#resell_lists').attr("style", "display:none;");
+}
+
+// 생활게시판 - 보관
+function keep() {
+    console.log('보관 게시판 clicked');
+    $('#cobuy_lists').attr("style", "display:none;");
+    $('#keep_lists').attr("style", "display:;");
+    $('#rent_lists').attr("style", "display:none;");
+    $('#resell_lists').attr("style", "display:none;");
+}
+
+// 생활게시판 - 대여
+function rent() {
+    console.log('대여 게시판 clicked');
+    $('#cobuy_lists').attr("style", "display:none;");
+    $('#keep_lists').attr("style", "display:none;");
+    $('#rent_lists').attr("style", "display:;");
+    $('#resell_lists').attr("style", "display:none;");
+}
+
+// 생활게시판 - 중고
+function resell() {
+    console.log('중고 게시판 clicked');
+    $('#cobuy_lists').attr("style", "display:none;");
+    $('#keep_lists').attr("style", "display:none;");
+    $('#rent_lists').attr("style", "display:none;");
+    $('#resell_lists').attr("style", "display:;");
+}
+
