784d0bf8cbf63233777e9e16891b9a2b06db4025
suuuum36 <lsm3191@snu.ac.kr>
2020-08-02 03:53:22 +0900

    show-almost done

--- snuDorm/feedpage/views.py
+++ snuDorm/feedpage/views.py
@@ -106,7 +106,7 @@ def showBoard(request, board, category):
                 
         # 전체글 버튼
         feeds = feeds.order_by('-created_at')
-        paginator = Paginator(feeds, 11)
+        paginator = Paginator(feeds, 1)
         page = request.GET.get('page')
         posts = paginator.get_page(page)
 
