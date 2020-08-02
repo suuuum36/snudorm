04232af67cca1da6d2a9c90dd12d3a88534d1f66
suuuum36 <lsm3191@snu.ac.kr>
2020-08-02 10:43:50 +0900

    show front-end / 오류확인요망

--- snuDorm/feedpage/static/feedpage/showboard.js
+++ snuDorm/feedpage/static/feedpage/showboard.js
@@ -16,3 +16,28 @@ BestButton.addEventListener('click', () => {
     BestButton.style.color = 'black';
     AllButton.style.color = '#7f7f7f';
 })
+
+// page numbers 효과
+
+var urlPath = window.location.href
+
+var num1Text = document.querySelector('.num1').innerText;
+var num1 = document.querySelector('.num1')
+var num2Text = document.querySelector('.num2').innerText;
+var num2  = document.querySelector('.num2')
+
+var num3Text = document.querySelector('.num3').innerText;
+var num3 = document.querySelector('.num3')
+
+console.log(num1Text);
+console.log(num3Text);
+console.log(urlPath);
+
+
+if(urlPath.indexOf(num1Text)!= -1) {
+    num1.style.color = '#016aff';
+} else if (urlPath.indexOf(num2Text)!= -1) {
+    num2.style.color = '#016aff';
+} else {
+
+}
