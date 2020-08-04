ff99ec27f3442e9371ed6c3971e195a955a3d296
suuuum36 <lsm3191@snu.ac.kr>
2020-08-04 17:07:20 +0900

    show list 수정 완료

--- snuDorm/feedpage/views.py
+++ snuDorm/feedpage/views.py
@@ -56,13 +56,13 @@ def get_feed(board, category):
 
 def get_pages(feeds, request):
     # 전체글 버튼
-    paginator = Paginator(feeds, 1)
+    paginator = Paginator(feeds, 11)
     page = request.GET.get('page')
     posts = paginator.get_page(page)
 
     # 베스트 버튼 
     best_feeds = feeds.order_by('-like_users')
-    paginator2 = Paginator(best_feeds, 1)
+    paginator2 = Paginator(best_feeds, 11)
     best_page = request.GET.get('best_page', 1)
     best_posts = paginator2.get_page(best_page)
     
