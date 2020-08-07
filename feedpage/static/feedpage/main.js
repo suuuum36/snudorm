475d349ffd38c246b8beb9b376f0933cbe419ce8
suuuum36 <lsm3191@snu.ac.kr>
2020-08-07 21:09:01 +0900

    base 수정중

--- feedpage/static/feedpage/main.js
+++ feedpage/static/feedpage/main.js
@@ -75,7 +75,7 @@ for (idx=0; idx < 11; idx++) {
     if(url.indexOf(category2[idx])!= -1) {
         categoryText[idx].style.color = '#016aff';
     } else {
-
+        categoryText[idx].style.color = '#7f7f7f';
     }
 }
 
@@ -153,6 +153,10 @@ if (url.indexOf("freeboard")!= -1 || urlPath == "" || urlPath == "feeds" )  {
         headerWhole.style.height ='0';
 } else if (url.indexOf("tori")!= -1 || url.indexOf("gong")!= -1 || url.indexOf("life")!= -1) {
         header3.style.height = '0';
+} else if (url.indexOf("bachelor")!= -1 || url.indexOf("master")!= -1 || url.indexOf("family")!= -1 || url.indexOf("bk")!= -1  ) {
+        header3.style.height ='60px';
 } else if (url.indexOf("select-option")!= -1 ) {
         headerWhole.style.height ='0';
+} else {
+
 }
